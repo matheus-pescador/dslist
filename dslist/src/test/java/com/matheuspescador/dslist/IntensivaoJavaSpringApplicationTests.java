577d32f3b852332eb2195e7ccc8a9d69d89e5bf8
Matheus <matheus.pescador2@gmail.com>
2023-11-13 20:22:29 -0400

    Project created

---
+++ dslist/src/test/java/com/matheuspescador/dslist/IntensivaoJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheuspescador.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntensivaoJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
